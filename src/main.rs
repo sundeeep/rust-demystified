96141c780dc525f8c2b2146f5e8f6bdf23c37c07
sundeeep <sandeepkumar25820@gmail.com>
2024-11-17 18:38:06 +0530

    files are deleted and cleaned!

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,4 @@
-// Import statements:
-use actix_web::{get, App, HttpServer, Responder};
-use std::io::Result;
 
-mod models;
-mod ownership;
-
-use ownership::strings::init_ownership;
-use ownership::strings;
-
-#[get("/")]
-async fn greet() -> impl Responder {
-    format!("Hello, drugbord.ai")
-    
-}
-
-#[actix_web::main]
-async fn main() -> Result<()> {
-    
-    const PORT: u16 = 8000;
-    const LOCALHOST: &str = "127.0.0.1";
-    println!();
-
-    init_ownership();
-    strings::main();
-
-
-
-
-    // println!("HTTP Server is running on port: {}", PORT);
-    
-    HttpServer::new(|| App::new().service(greet))
-        .bind((LOCALHOST, PORT))? // returns "Result" => ?
-        .workers(2)
-        .run()
-        .await
+fn main() {
+    print!("Hello from Sundeep, learning Rust!");
 }
--- src/ownership/README.md
+++
@@ -1,7 +0,0 @@
-# Ownership in Rust Language
-
-## What is Ownership?
-
-## Why does Ownership is introduced to Rust?
-
-## How to make advantage out of this Ownership feature?
